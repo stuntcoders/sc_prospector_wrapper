a6aea775aa753face1ccc53b75ca6138cc4dabee
Dejan Jacimovic <dejan.jacimovic@stuntcoders.com>
2018-02-26 14:17:39 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 wget https://raw.githubusercontent.com/stuntcoders/sc_prospector_wrapper/master/company.sh
 sudo chmod +x ./company.sh
-sudo cp ./company.sh /usr/local/bin/company
+sudo mv ./company.sh /usr/local/bin/company
