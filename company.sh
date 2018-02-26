0634cb4d2b39bbb14a89290716f1bb6220c2cbd4
Dejan Jacimovic <dejan.jacimovic@stuntcoders.com>
2018-02-26 14:30:43 +0100

    Make script run silently

--- company.sh
+++ company.sh
@@ -60,7 +60,7 @@ else
 	exit 1
 fi
 
-curl "https://company.clearbit.com/v2/companies/find?domain=$1" -u $CLEARBIT_KEY | jq -r '.metrics' | in2csv -f json >> output.csv
+curl --silent "https://company.clearbit.com/v2/companies/find?domain=$1" -u $CLEARBIT_KEY | jq -r '.metrics' | in2csv -f json >> output.csv
 
-curl "https://api.hunter.io/v2/domain-search?domain=$1&api_key=$HUNTER_KEY" | jq -r '.data.emails' | in2csv -f json | csvcut -c value,type,confidence >> output.csv
+curl --silent "https://api.hunter.io/v2/domain-search?domain=$1&api_key=$HUNTER_KEY" | jq -r '.data.emails' | in2csv -f json | csvcut -c value,type,confidence >> output.csv
 
